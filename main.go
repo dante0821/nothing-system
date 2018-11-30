769dc14635f08cda9e796893568362f0079ba297
shizhicheng <dante0821@163.com>
2018-11-30 09:44:32 +0800

    update

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"net/http"
 	"time"
+	"net/http"
+	"awesomeProject/MySystem/middlewares"
 )
 
 func main() {
@@ -22,38 +23,35 @@ func main() {
 }
 
 func route(e *echo.Echo) {
-
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "method=${method}, uri=${uri}, status=${status},form=${form}\n",
 	}))
 	//group := e.Group("/")
 	e.POST("login", Login)
+	e.GET("user_info", GetCurrentUserInfo, middlewares.KeyAuth())
 	e.File("/", "web/html/login.html")
 	e.File("/index", "web/html/index.html")
 	e.Static("/src", "web/src")
+	e.Static("/lay", "web/src/lay")
+	e.Static("/css", "web/src/css")
 	e.Static("/html", "web/html")
 }
 
 func Login(c echo.Context) error {
-	loginInfo := &LoginInfo{}
-	if err:= c.Bind(loginInfo); err != nil {
-		return c.JSON(http.StatusBadRequest, &Rsp{"请求体错误"})
-	}
-
-	if loginInfo.Username != "szc" {
+	c.Request().ParseForm()
+	if c.FormValue("username") != "szc" {
 		return c.JSON(http.StatusBadRequest, &Rsp{"账号错误"})
 	}
-	if loginInfo.Username != "szc" {
+	if c.FormValue("password") != "szc" {
 		return c.JSON(http.StatusBadRequest, &Rsp{"密码错误"})
 	}
 	return c.JSON(http.StatusOK, nil)
 }
 
-type Rsp struct {
-	Msg string `json:"msg"`
+func GetCurrentUserInfo(c echo.Context) error {
+	return c.JSON(http.StatusOK, nil)
 }
 
-type LoginInfo struct {
-	Username string
-	Password string
+type Rsp struct {
+	Msg  string `json:"msg"`
 }
